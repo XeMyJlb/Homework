0a4848cac5d8720c9b6472e0933dc77e72283694
hemul <xemyjlb28@gmail.com>
2022-05-16 15:58:55 +0300

    task1

---
+++ hm1task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a1 = 5;
+int b1 = 7;
+int a2 = 2;
+int b2 = 10;
+int a3 = -9;
+int b3 = -3;
+
+int max1 = a1;
+if (b1 > max1) max1 = b1;
+{
+    Console.WriteLine(max1);
+
+}
+
+int max2 = a2;
+if (b2 > max2) max2 = b2;
+{
+    Console.WriteLine(max2);
+    
+}
+
+int max3 = a3;
+if (b3 > max3) max3 = b3;
+{
+    Console.WriteLine(max3);
+    
+}
