95a84e72b8d2c6c1d1015980bc38bc605b5d100d
hemul <xemyjlb28@gmail.com>
2022-05-19 15:31:15 +0300

    hm2task1

---
+++ hm2task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA > 99 & numberA < 1000)
+{
+int numberOne = numberA / 100;
+int numberTwo = (numberA - numberOne * 100) / 10;
+int numberThree = numberA - numberOne * 100 - numberTwo * 10;
+Console.WriteLine(numberTwo);
+}
+else
+{
+System.Console.WriteLine("Вы ввели не верное число");
+}
