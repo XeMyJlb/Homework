51e2d1b00d004321b9302ecbe07a1060383fb311
hemul <xemyjlb28@gmail.com>
2022-05-19 15:45:31 +0300

    hm2task2

---
+++ hm2task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+System.Console.WriteLine("Введите ваше число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 99)
+{
+System.Console.WriteLine("Третьего чилса нет");
+}
+else
+{
+Console.WriteLine(number > 99 ? number.ToString()[2]:'-');
+}
+//P.S Не знал как решить данную задачу и загуглировал. 
