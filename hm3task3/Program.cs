635e0a003ca2086670c61b15ca3b04126dcf14eb
hemul <xemyjlb28@gmail.com>
2022-05-24 12:53:59 +0300

    hm3task3

---
+++ hm3task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result;
+Console.Write($"{number} - > ");
+
+for (int i = 1; i <= number; i++)
+{
+result = i * i * i;
+Console.Write(result);
+if (i != number)
+{
+Console.Write(" , ");
+}
+}
