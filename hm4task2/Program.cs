e4a678deff661e2a6a0833a9b4134069a96d559e
hemul <xemyjlb28@gmail.com>
2022-05-26 17:14:35 +0300

    hm4task2

---
+++ hm4task2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+System.Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+while (a > 0)
+{
+b = b + a % 10;
+a = a /10 ;
+}
+Console.WriteLine(b); 
