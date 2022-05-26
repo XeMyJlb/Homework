774343af890dfdcb7b75caea780bfba3287c7940
hemul <xemyjlb28@gmail.com>
2022-05-26 17:14:48 +0300

    hm4task3

---
+++ hm4task3/Program.cs
@@ -0,0 +1,34 @@
+﻿//29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+void FillArray(int[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        array[index] = new Random().Next(0, 10);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write(array[i]);
+        }
+
+    }
+    Console.Write("]");
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
