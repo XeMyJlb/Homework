c8afbc6ea96ded90a7bd1d4550bd8c358a12234e
XeMyJlb <103940835+XeMyJlb@users.noreply.github.com>
2022-06-13 20:08:18 +0300

    hm9task1-2

---
+++ hm9task2.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSum(m, n, temp=0);
+
+void PrintSum(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write("Сумма натуральных чисел: "+ summ);
+    return;
+  }
+  PrintSum(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
